5f81883179fe1437e4ebb445396a34946d64a273
Ashwin <ashwinmariodsilva@gmail.com>
2024-06-26 11:11:37 +0530

    refactor: PhotosListViewController

--- LoremPicsum/PhotosListViewController.swift
+++ LoremPicsum/PhotosListViewController.swift
@@ -11,11 +11,24 @@ class PhotosListViewController: UIViewController {
     
     // MARK: - Properties
     
+    private let viewModel: ViewModel
+    
     private lazy var photosListView: PhotosListView = {
         let view = PhotosListView()
         view.delegate = self
         return view
     }()
+    
+    // MARK: - Init
+    
+    init() {
+        self.viewModel = .init()
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     // MARK: - Lifecycle
     
@@ -32,21 +45,38 @@ class PhotosListViewController: UIViewController {
 
 extension PhotosListViewController: PhotosListViewDelegate {
     func didSelect(_ photo: Photo, isChecked: Bool) {
-        let alertTitle: String?
-        let alertMessage: String?
-        
         if isChecked {
-            alertTitle = "Info"
-            alertMessage = """
-            Author: \(photo.author ?? "NA")
-            """
+            showPhotoInfo(for: photo)
         } else {
-            alertTitle = nil
-            alertMessage = "Tick the checkbox to view info"
+            showCheckboxAlert()
         }
+    }
+}
+
+// MARK: - Methods
+
+extension PhotosListViewController {
+    private func showPhotoInfo(for photo: Photo) {
+        let (title, message) = viewModel.alertInfo(for: photo)
         
-        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .alert)
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
         alert.addAction(UIAlertAction(title: "Ok", style: .cancel))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func showCheckboxAlert() {
+        let alert = UIAlertController(
+            title: nil,
+            message: "Tick the checkbox to view info",
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: "Ok", style: .cancel))
+        
         self.present(alert, animated: true, completion: nil)
     }
 }
---
+++ LoremPicsum/PhotosListViewController_ViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  PhotosListViewController_ViewModel.swift
+//  LoremPicsum
+//
+//  Created by Ashwin D'Silva on 26/06/24.
+//
+
+import Foundation
+
+extension PhotosListViewController {
+    class ViewModel {
+        func alertInfo(for photo: Photo) -> (title: String, message: String?) {
+            let title: String = "Info"
+            let message: String? = {
+                guard let author = photo.author, let width = photo.width, let height = photo.height else {
+                    return nil
+                }
+                
+                return """
+                Author: \(author)
+                Image dimension: \(width)x\(height)
+                """
+            }()
+            
+            return (title, message)
+        }
+    }
+}
